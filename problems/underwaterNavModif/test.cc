12ff1dd498ce5fc9df43e5e4d566cb5442102921
lackofcheese <lackofcheese@gmail.com>
2013-10-24 01:44:47 +1000

    Removed silly test file.

--- problems/underwaterNavModif/test.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
-int main(int argc, char** argv) {
-    po::variables_map vm();
-}
